2e307a1951da53a61d3723d80954d68ad75e76fd
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-22 18:22:12 -0500

    Nullish coalescing operator ??

--- script.js
+++ script.js
@@ -53,33 +53,41 @@ const restaurant = {
   },
 };
 
-console.log('---- OR ----');
-// Use ANY data type, return ANY data type, short-circuiting
-console.log(3 || 'Isaac');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
 
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// console.log('---- OR ----');
+// // Use ANY data type, return ANY data type, short-circuiting
+// console.log(3 || 'Isaac');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-console.log('---- AND ----');
-console.log(0 && 'Isaac');
-console.log(7 && 'Isaac');
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-console.log('Hello' && 23 && null && 'Isaac');
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
 
-// Practical example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
+// console.log('---- AND ----');
+// console.log(0 && 'Isaac');
+// console.log(7 && 'Isaac');
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// console.log('Hello' && 23 && null && 'Isaac');
+
+// // Practical example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 // // 1) Destructuring
 
