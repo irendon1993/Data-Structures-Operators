4ac26f68621cb1cf0a5b3b25fffbdb69012443cf
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-23 10:48:11 -0500

    for of loop

--- script.js
+++ script.js
@@ -53,6 +53,14 @@ const restaurant = {
   },
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}}`);
+}
+
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -72,90 +80,90 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-// 1. Create one player array for each team (variables 'players1' and 'players2')
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// // 1. Create one player array for each team (variables 'players1' and 'players2')
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-// 2. The first player in any player array is the goalkeeper and the others are field players.
-// For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers')
-//  with all the remaining 10 field players
-const [gk, fieldPlayers] = game.players;
-console.log(gk, fieldPlayers);
+// // 2. The first player in any player array is the goalkeeper and the others are field players.
+// // For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers')
+// //  with all the remaining 10 field players
+// const [gk, fieldPlayers] = game.players;
+// console.log(gk, fieldPlayers);
 
-// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players.
-// So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// // 4. During the game, Bayern Munich (team 1) used 3 substitute players.
+// // So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
 
-// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
+// // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
 
-// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
-// and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
+// // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+// // and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
 
-// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-// printGoals('Davies', 'Muller');
+// // printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// // printGoals('Davies', 'Muller');
 
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
-// 7. The team with the lower odd is more likely to win.
-// Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// // 7. The team with the lower odd is more likely to win.
+// // Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
 
-// const rest1 = {
+// // const rest1 = {
 //   name: 'Capri',
 //   numGuests: 0,
 //   // numGuests: 20,
