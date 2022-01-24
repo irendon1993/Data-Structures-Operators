e800a204323e4cbab9ecbb46274cacc391d58b14
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-24 13:38:52 -0500

    Maps and Sets challenge

--- script.js
+++ script.js
@@ -54,110 +54,115 @@ const restaurant = {
   },
 };
 
-//////////////////////////////////////
-// Coding Challenge #2
+///////////////////////////////////////
+// Coding Challenge #3
 
 /* 
-Let's continue with our football betting app!
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
 
 
 
-
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties,
- and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
 
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-
-const orderSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
 ]);
 
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct :D'],
-  [false, 'try again'],
-]);
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
 
-console.log(question);
+// console.log(gameEvents);
 
-// Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// const events = new Set();
 
-// Quiz app
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// for (const [, value] of gameEvents) {
+//   events.add(value);
+// }
+
+// console.log(events);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair.
+//  So remove this event from the game events log.
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Print the following string to the console:
+// "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+console.log(
+  `An event happend, on average every ${90 / gameEvents.size} minutes`
+);
+
+// 4. Loop over the events and log them to the console, marking whether it's in the first half
+//  or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+
+for (const [key, event] of gameEvents) {
+  if (key <= 45) {
+    console.log(`[FIRST HALF] ${key}: ${event}`);
+  } else {
+    console.log(`[SECOND HALF] ${key}: ${event}`);
+  }
 }
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
 
-console.log(question.get(question.get('correct') === answer));
+// const orderSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
 
-// Convert map to array
-console.log([...question]);
-// console.log(question.entries);
-console.log([...question.keys()]);
-console.log([...question.values]);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct :D'],
+//   [false, 'try again'],
+// ]);
+
+// console.log(question);
+
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// // Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert map to array
+// console.log([...question]);
+// // console.log(question.entries);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 
 // const rest = new Map();
 // rest.set('name', 'Cassariano');
@@ -216,6 +221,65 @@ console.log([...question.values]);
 // );
 
 // console.log(new Set('isaacrendon').size);
+
+//////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties,
+ and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
 // // 1. Loop over the game.scored array and print each player name to the console,
 // // along with the goal number (Example: "Goal 1: Lewandowski")
