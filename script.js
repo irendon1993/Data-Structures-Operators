6477f8b878410e9058586e9d91889ec1db4994cb
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-24 11:21:58 -0500

    Destrucurig challenge #2

--- script.js
+++ script.js
@@ -53,31 +53,134 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
-if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log(restaurant.openingHours.mon.open);
 
-// Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+//////////////////////////////////////
+// Coding Challenge #2
 
-let openStr = `We are open on ${properties.length} days: `;
+/* 
+Let's continue with our football betting app!
 
-for (const day of properties) {
-  openStr += `${day},`;
+
+
+
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties,
+ and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+// for (const [goal, player] of game.scored.entries()) {
+//   console.log(`Goal ${goal + 1}: ${player}`);
+// }
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages,
+//   you can go check if you don't remember)
+
+// const oddsValues = Object.values(game.odds);
+// console.log(oddsValues);
+
+// let oddSum = 0;
+
+// for (const odd of oddsValues) {
+//   oddSum += odd;
+// }
+
+// console.log(oddSum / oddsValues.length);
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw").
+//  HINT: Note how the odds and the game objects have the same property names 😉
+
+// console.log(`Odd of victory ${game.team1}: ${game.odds.team1}`);
+// console.log();
+// console.log(`Odd of victory ${game.team2}: ${game.odds.team2}`);
+
+console.log(Object.entries(game.odds));
+console.log(game.team1);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
 }
-console.log(openStr);
 
-//Proerty VALUES
-const values = Object.values(openingHours);
-console.log(values);
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
 
-//Entire object values
-const entries = Object.entries(openingHours);
-console.log(entries);
+// // Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}`);
-}
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (const day of properties) {
+//   openStr += `${day},`;
+// }
+// console.log(openStr);
+
+// //Proerty VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// //Entire object values
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
 
 // // WITH opetional chaining
 // console.log(restaurant.openingHours.mon?.open);
