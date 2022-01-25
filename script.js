c8bc88aa5aa08104d5debaeee137ec5f1bf253aa
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-25 15:11:16 -0500

    Strings challenge

--- script.js
+++ script.js
@@ -54,6 +54,82 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+
+
+THIS TEST DATA (pasted to textarea)
+
+
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+// PROBLEM
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+//EXAMPLE
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// DATA
+
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+// ALGORITHM
+/*
+text area will receive list
+function takes in a list of vaiables
+trims each variable
+converts into lower case
+converts into camelcase
+button will execute conversion function
+console.log each variable
+
+*/
+
+// CODE
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const textButton = document.querySelector('button');
+
+textButton.addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    // console.log(row, first, second);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
 // // Split and join
 // console.log('a+very+nice+string'.split('+'));
 
